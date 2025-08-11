13d3c81071c820f8c4b6682e2e55a369de63955b
theworkguy <shalatoby@gmail.com>
2025-08-11 02:02:42 +0200

    Fix SVG background syntax error

--- src/pages/LatestSongPage.tsx
+++ src/pages/LatestSongPage.tsx
@@ -27,7 +27,12 @@ const LatestSongPage: React.FC = () => {
         {/* Enhanced Background with Animated Particles */}
         <div className="fixed inset-0">
           <div className="absolute inset-0 bg-gradient-to-br from-purple-900 via-blue-900 to-black"></div>
-          <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width=\"60\" height=\"60\" viewBox=\"0 0 60 60\" xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cg fill=\"none\" fill-rule=\"evenodd\"%3E%3Cg fill=\"%23ffffff\" fill-opacity=\"0.03\"%3E%3Ccircle cx=\"30\" cy=\"30\" r=\"2\"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-40"></div>
+          <div className="absolute inset-0 opacity-40">
+            <div className="absolute inset-0" style={{
+              backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='0.03'%3E%3Ccircle cx='30' cy='30' r='2'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
+              backgroundRepeat: 'repeat'
+            }}></div>
+          </div>
           
           {/* Floating Elements */}
           <div className="absolute top-20 left-10 w-2 h-2 bg-cyan-400 rounded-full animate-pulse opacity-60"></div>
